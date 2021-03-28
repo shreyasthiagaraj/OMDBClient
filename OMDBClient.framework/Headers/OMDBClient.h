d53050f52ce8e2f9d0a184068270206e399786b2
Shreyas <shreyas.thiagaraj@gmail.com>
2021-03-28 18:13:39 -0400

    Add files via upload

---
+++ OMDBClient.framework/Headers/OMDBClient.h
@@ -0,0 +1,18 @@
+//
+//  OMDBClient.h
+//  OMDBClient
+//
+//  Created by Shreyas Thiagarajasubramanian on 3/28/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OMDBClient.
+FOUNDATION_EXPORT double OMDBClientVersionNumber;
+
+//! Project version string for OMDBClient.
+FOUNDATION_EXPORT const unsigned char OMDBClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OMDBClient/PublicHeader.h>
+
+
